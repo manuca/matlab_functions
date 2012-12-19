89a7859aa0016a279f7cafa60e1fd9b62d1949f5
Manuel Barros Reyes <manuca@gmail.com>
2012-12-19 11:11:43 -0300

    Algunas funciones

---
+++ energia.m
@@ -0,0 +1,8 @@
+function [energiaout] = energia(senal,Ts)
+% [energiaout] = energia(senal,Ts)
+%
+% Recibe una senal y su Tiempo de muestreo. calcula la energia
+% como sum(X*conj(x))*Ts
+
+energiaout = sum(senal.*conj(senal))*Ts;
+return
---
+++ generar_senial.m
@@ -0,0 +1,17 @@
+function [secuencia, s] = generar_senial(n)
+% [secuencia, s] = generar_senial(n)
+% secuencia: Secuencia aleatoria 4-PAM
+% s: Señal modulada por pulso formador
+%
+% Genera la señal real con n símbolos
+  A = 1;
+  ts = 1/10;
+
+  secuencia = secuencia_pam(n, A);
+  secuencia = upsample(secuencia, 1/ts);
+
+  x = -1:ts:1;
+  p = sinc(x);
+
+  s = conv(secuencia, p);
+end
