6b1db57250542c9590e96b28cff942123f2d8146
Manuel Barros Reyes <manuca@gmail.com>
2012-12-19 11:33:37 -0300

    Correciones compatibilidad ML7.6 y default values

---
+++ corrida.m
@@ -0,0 +1,14 @@
+function corrida(n)
+  % Corre la simulación
+  % n: Número de símbolos a transmitir (default: 10)
+
+  if(nargin == 0)
+    n = 10;
+  end
+
+  % Apertura de la PAM
+  A = 0.85;
+
+  x_n = secuencia_pam(n, A);
+  stem(x_n);
+end
--- secuencia_pam.m
+++ secuencia_pam.m
@@ -6,7 +6,18 @@ function s = secuencia_pam(n, A)
   %
   % Devuelve una secuencia aleatoria de n elementos
   % con valores posibles [-3A, -A, A, 3A] de una 4-PAM
+  % n: Elementos (default: 10)
+  % A: Apertura de la PAM (default: 1)
 
-  s = (randi(4, 1, n)*2 - 5);
+  if (nargin == 0)
+    n = 10;
+    A = 1;
+  elseif (nargin == 1)
+    A = 1;
+  end
+
+  randi = ceil(rand(1, n) * 4);
+
+  s = (randi*2 - 5);
   s = s.*A;
 end
