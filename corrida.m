ee56dbf18ee13b6b10ed74561b2181d25287a31c
Manuel Barros Reyes <manuca@gmail.com>
2012-12-19 22:52:40 -0300

    Deteccion

--- corrida.m
+++ corrida.m
@@ -15,14 +15,14 @@ function corrida(n)
 
   subplot(5, 1, 1);
   stem(x_n);
-  title('Secuencia 4PAM');
+  title('Secuencia 4PAM - x[n]');
 
   x_t = modular_pam(x_n, ts);
   x_t = extraer(x_t, 10);
 
   subplot(5, 1, 2);
   stem(x_t);
-  title('Señal modulada');
+  title('Señal modulada - x(t)');
 
   subplot(5, 1, 3);
   stem(awgn(x_t, 18.57, 'measured'));
@@ -33,17 +33,15 @@ function corrida(n)
   deteccion = conv(x_t, pulso_formador/norma_pulso);
   deteccion = extraer(deteccion, 10);
 
-  fprintf('Energía filtro adaptado: %6.2f\n', norma_pulso);
-
   subplot(5, 1, 4);
   stem(deteccion);
-  title('Señal luego de filtro adaptado');
+  title('Señal luego de filtro adaptado - y(t)');
 
   y_t = deteccion(1:ceil(1/ts):end-10);
 
   subplot(5, 1, 5);
   stem(y_t);
-  title('Señal detectada - y(t)');
+  title('Señal detectada - y[k]');
 end
 
 function s = extraer(signal, n)
