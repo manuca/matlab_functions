0b87b07b296043f24db03151c72138fc4f1fdce0
Manuel Barros Reyes <manuca@gmail.com>
2012-12-20 00:30:23 -0300

    Metodos para plotear extraidos a otra funcion

---
+++ graficar_seniales.m
@@ -0,0 +1,22 @@
+% vim: ff=unix fileencoding=latin1
+function graficar_seniales(x_n, x_t, y_t, mfo, y_n)
+  subplot(5, 1, 1);
+  stem(x_n);
+  title('Secuencia 4PAM - x[n]');
+
+  subplot(5, 1, 2);
+  stem(x_t);
+  title('Señal modulada - x(t)');
+
+  subplot(5, 1, 3);
+  stem(y_t);
+  title('Señal modulada con ruido 18.57 dB - y(t)');
+
+  subplot(5, 1, 4);
+  stem(mfo);
+  title('Señal luego de filtro adaptado - mfo(t)');
+
+  subplot(5, 1, 5);
+  stem(y_n);
+  title('Señal detectada - y[k]');
+end
--- simular_awgn.m
+++ simular_awgn.m
@@ -1,5 +1,5 @@
 % vim: ff=unix fileencoding=latin1
-function [x_n, x_t, y_t, mfo, y_n] = corrida(n)
+function [x_n, x_t, y_t, mfo, y_n] = simular_awgn(n)
   % Corre la simulación
   % n: Número de símbolos a transmitir (default: 10)
   % Retorna
@@ -17,42 +17,28 @@ function [x_n, x_t, y_t, mfo, y_n] = corrida(n)
   A = 0.85;
   ts = 1/10;
 
+  % x_n
   x_n = secuencia_pam(n, A);
 
-  subplot(5, 1, 1);
-  stem(x_n);
-  title('Secuencia 4PAM - x[n]');
-
+  % x_t
   x_t = modular_pam(x_n, ts);
   x_t = extraer(x_t, 10);
 
-  subplot(5, 1, 2);
-  stem(x_t);
-  title('Señal modulada - x(t)');
-
+  % y_t
   y_t = awgn(x_t, 18.57, 'measured');
-  subplot(5, 1, 3);
-  stem(y_t);
-  title('Señal modulada con ruido 18.57 dB - y(t)');
 
+  % mfo
   pulso_formador = sinc([-1:ts:1]);
   norma_pulso = energia(pulso_formador, ts);
-  mfo = conv(x_t, pulso_formador/norma_pulso) * ts ; % Aplico esta normalización
+  mfo = conv(x_t, pulso_formador/norma_pulso) * ts ; % Normalizo con ts
   mfo = extraer(mfo, 10);
 
-  subplot(5, 1, 4);
-  stem(mfo);
-  title('Señal luego de filtro adaptado - mfo(t)');
-
+  % y_n
   y_n = mfo(1:ceil(1/ts):end-9);
-
-  subplot(5, 1, 5);
-  stem(y_n);
-  title('Señal detectada - y[k]');
 end
 
 function s = extraer(signal, n)
   % Eliminamos n primeros elementos 
-  % y n últimos elementos
+  % y n-1 últimos elementos
   s = signal(n:(end-n-1));
 end
