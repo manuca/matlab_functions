1af0cdacb4754ebac666f1cf8c4fb213413c8587
Manuel Barros Reyes <manuca@gmail.com>
2012-12-23 17:39:47 -0300

    Funciona sin awgn

--- simular_awgn.m
+++ simular_awgn.m
@@ -22,7 +22,7 @@ function [x_n, x_t, y_t, mfo, y_n] = simular_awgn(n)
 
   % x_t
   x_t = modular_pam(x_n, ts);
-  x_t = x_t(11:end);
+  x_t = x_t(11:(end-10));
 
   % y_t
   y_t = x_t;
@@ -34,10 +34,11 @@ function [x_n, x_t, y_t, mfo, y_n] = simular_awgn(n)
   mfo = conv(x_t, pulso_formador/norma_pulso) * ts ; % Normalizo con ts
   % Nro mágico en base a las diferecias en los gráficos
   factor_normalizacion_empirico = 1;
-  mfo = mfo(1:end-7) * factor_normalizacion_empirico;
+  mfo = mfo(11:(end-10)) * factor_normalizacion_empirico;
 
   % y_n
-  y_n = mfo(1:ceil(1/ts):end-9);
+  periodo_discreto = ceil(1/ts);
+  y_n = mfo(1:periodo_discreto:end);
 
   % error_count = detectar_errores_pam(x_n, y_n, A);
   % disp(sprintf('Se detectaron %i errores en %i elementos', error_count, length(x_n)));
