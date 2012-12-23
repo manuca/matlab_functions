28c48cbd7e164596f3475749ab438fcd6b1f03f3
Manuel Barros Reyes <manuca@gmail.com>
2012-12-23 17:46:49 -0300

    Comments

--- simular_awgn.m
+++ simular_awgn.m
@@ -32,7 +32,7 @@ function [x_n, x_t, y_t, mfo, y_n] = simular_awgn(n)
   pulso_formador = sinc([-1:ts:1]);
   norma_pulso = energia(pulso_formador, ts);
   mfo = conv(x_t, pulso_formador/norma_pulso) * ts ; % Normalizo con ts
-  % Nro mágico en base a las diferecias en los gráficos
+  % Nro mágico para corregir diferecias en los gráficos
   factor_normalizacion_empirico = 1;
   mfo = mfo(11:(end-10)) * factor_normalizacion_empirico;
 
@@ -40,8 +40,8 @@ function [x_n, x_t, y_t, mfo, y_n] = simular_awgn(n)
   periodo_discreto = ceil(1/ts);
   y_n = mfo(1:periodo_discreto:end);
 
-  % error_count = detectar_errores_pam(x_n, y_n, A);
-  % disp(sprintf('Se detectaron %i errores en %i elementos', error_count, length(x_n)));
+  error_count = detectar_errores_pam(x_n, y_n, A);
+  disp(sprintf('Se detectaron %i errores en %i elementos', error_count, length(x_n)));
 end
 
 % vim: ff=unix fileencoding=latin1
