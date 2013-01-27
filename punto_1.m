0dacafdf847c6e6b500022f999de6c96762ad418
Manuel Barros Reyes <manuca@gmail.com>
2013-01-27 14:38:22 -0300

    Parametrize no. of symbols transmitted

--- punto_1.m
+++ punto_1.m
@@ -1,9 +1,19 @@
-function punto_1()
+function punto_1(simbolos, show_plot)
   % Simula la transmisión de una señal 4-PAM a través de un canal ideal
   % y su detección con un filtro adaptado.
+  
+  if (nargin == 0)
+    simbolos = 100;
+    show_plot = false;
+  elseif (nargin == 1)
+    show_plot = false;
+  end
 
-  [xn x_up xt yt mfo yn, errores_ubic] = simular_awgn(1000);
-  graficar_seniales(xn, x_up, xt, yt, mfo, yn, errores_ubic);
+  [xn x_up xt yt mfo yn, errores_ubic] = simular_awgn(simbolos);
+
+  if (show_plot)
+    graficar_seniales(xn, x_up, xt, yt, mfo, yn, errores_ubic);
+  end
 end
 
 % vim: ff=unix fileencoding=latin1
